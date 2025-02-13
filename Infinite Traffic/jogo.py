efadcd5571f96e4382c3d0927d740fbae190f62d
Fernando Passos <luisfernandopassos84@gmail.com>
2025-02-13 15:59:41 -0300

    inserindo timer

--- Infinite Traffic/jogo.py
+++ Infinite Traffic/jogo.py
@@ -6,10 +6,11 @@ x = 425
 y = 150
 
 posicao_x = 425
-posicao_y = 1000
-posicao_y_1 = 1000
-posicao_y_2 = 1000
-
+posicao_y = 800
+posicao_y_1 = 800
+posicao_y_2 = 800
+timer = 0
+tempo_segundo = 0 
 
 velocidade = 20
 velocidade_outros = 30
@@ -29,7 +30,6 @@ carro4 = pygame.image.load('imagens/carro4.png')
 carro4 = pygame.transform.scale(carro4, (165, 350))
 
 carro5 = pygame.image.load('imagens/carro5.png')
-carro5 = pygame.transform.scale(carro5, (165, 350))
 
 carro6 = pygame.image.load('imagens/carro6.png')
 carro6 = pygame.transform.scale(carro6, (165, 350))
@@ -46,6 +46,7 @@ carro9 = pygame.transform.scale(carro9, (165, 350))
 carro10 = pygame.image.load('imagens/carro10.png')
 carro10 = pygame.transform.scale(carro10, (165, 350))
 
+font = pygame.font.SysFont('PressStart2P.ttf', 60)
 janela = pygame.display.set_mode((1000, 1000))
 pygame.display.set_caption("INFINITE TRAFFIC")
 
@@ -59,7 +60,6 @@ while janela_aberta:
         if event.type == pygame.QUIT:
             janela_aberta = False
 
-    # Movimentação do círculo (continua enquanto a tecla for pressionada)
     comandos = pygame.key.get_pressed()
 
     if comandos[pygame.K_UP]:
@@ -68,29 +68,40 @@ while janela_aberta:
     if comandos[pygame.K_DOWN]:
         y += velocidade
 
-    if comandos[pygame.K_LEFT] and x>=95 : # So pode mover para esquerda ate 95px
+    if comandos[pygame.K_LEFT] and x >= 95:
         x -= velocidade
 
-    if comandos[pygame.K_RIGHT] and x<=725: #So pode mover para direita ate 725px
+    if comandos[pygame.K_RIGHT] and x <= 725:
         x += velocidade
     
-    if (posicao_y <= -350) and  (posicao_y_1 <= -350) and (posicao_y_2 <= -350):
+    if (posicao_y <= -350) and (posicao_y_1 <= -350) and (posicao_y_2 <= -350):
         posicao_y = randint(800, 2000)
         posicao_y_1 = randint(800, 2000)
-        posicao_y_2 = randint(800,2000)
+        posicao_y_2 = randint(800, 2000)
 
+    if timer < 20:
+        timer += 1
+    else:
+        tempo_segundo += 1
+        timer = 0
 
     posicao_y -= velocidade_outros
-    posicao_y_1 -= velocidade_outros +2
-    posicao_y_2 -= velocidade_outros +10
+    posicao_y_1 -= velocidade_outros + 2
+    posicao_y_2 -= velocidade_outros + 10
 
     # Atualiza a janela
-    janela.blit(fundo, (0,0))
-    janela.blit(carro, (x,y))
-
+    janela.blit(fundo, (0, 0))
+    janela.blit(carro, (x, y))
     janela.blit(carro1, (posicao_x, posicao_y))
     janela.blit(carro2, (posicao_x - 300, posicao_y_1))
     janela.blit(carro4, (posicao_x + 300, posicao_y_2))
+
+    texto_sombra = font.render(f"Tempo: {tempo_segundo}", True, (20, 20, 20))
+    texto = font.render(f"Tempo: {tempo_segundo}", True, (255, 200, 0))  
+
+    pos_texto = texto.get_rect(center=(120, 50))
+    janela.blit(texto_sombra, (pos_texto.x + 2, pos_texto.y + 2)) 
+    janela.blit(texto, pos_texto)
 
     pygame.display.update()
 
--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 Jogo destinado a disciplina de game design. Utilizamos apenas python.
 >
 # Execução
-**Certifique-se que o python está instalado na sua máquina,** depois basta executar o comando para que a biblioteca pygame seja instalada pela pip:
+**Certifique-se que o python está instalado na sua máquina,** depois basta executar o comando para que a biblioteca pygame seja instalada pelo pip:
 ```
 python -m pip install pygame
 ```
